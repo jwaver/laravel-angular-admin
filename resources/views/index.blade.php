615f606d56923641ac6c41e7022792a223df5438
jamesjomuad <jomuad_james@yahoo.com>
2017-08-02 09:53:05 +0800

    update base url

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,11 @@
 <!doctype html>
 <html ng-app="app">
 <head>
+    <base href="laravel-angular-admin/public">
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <link rel="stylesheet" href="{!! elixir('css/final.css') !!}">
+    <link rel="stylesheet" href="..{!! elixir('css/final.css') !!}">
     <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic'>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
@@ -23,6 +24,6 @@
         <div ui-view="layout"></div>
         <div class="control-sidebar-bg"></div>
     </div>
-    <script src="{!! elixir('js/final.js') !!}" async defer></script>
+    <script src="..{!! elixir('js/final.js') !!}" async defer></script>
 </body>
 </html>
